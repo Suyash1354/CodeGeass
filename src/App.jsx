e2ca432834d09461ee6e771c22dc19f5225e0e1d
Suyash Vinod Patil <patilsuyash31@gmail.com>
2026-03-01 20:47:25 +0530

    Initialize React + Vite project
    Add initial project scaffold for a React app using Vite: index.html, src (main.jsx, App.jsx, index.css, scenes), public asset (hands.png), package.json, vite.config.js, ESLint config, README.md and .gitignore. Provides basic setup for development with React, HMR, linting rules and starter scene components.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+  import FirstScene from './scenes/FirstScene'
+  import SecondScene from './scenes/SecondScene'
+
+
+  const App = () => {
+
+    
+    return (
+      <main className='w-full min-h-screen'>
+        <FirstScene/>
+        <SecondScene/>
+      </main>
+    )
+  }
+
+  export default App
---
+++ src/scenes/SecondScene.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SecondScene = () => {
+  return (
+    <main className='w-full h-screen bg-amber-800'></main>
+  )
+}
+
+export default SecondScene
